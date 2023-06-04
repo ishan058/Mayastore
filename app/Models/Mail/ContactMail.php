7b392da97bd332e8e80eed14b90637f23ad56fdd
ishan058 <ishankhadkaik246@gmail.com>
2023-06-04 13:07:39 +0545

    solved

--- app/Models/Mail/ContactMail.php
+++ app/Models/Mail/ContactMail.php
@@ -1,11 +1,35 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Mail;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
 
-class ContactMail extends Model
+class ContactMail extends Mailable
 {
-    use HasFactory;
-}
+    use Queueable, SerializesModels;
+
+    public $details;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+    /**
+     * Build the message
+     *  @return $this
+     */
+
+    public function build()
+    {
+        return $this->subject('Contact Message')->view('emails.ContactMail');
+    }
+ }
