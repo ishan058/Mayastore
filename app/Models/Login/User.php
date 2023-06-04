4dc414668f9094e7dec923a42b096c0db929bc25
ishan058 <ishankhadkaik246@gmail.com>
2023-06-04 13:03:52 +0545

    userdetails

--- app/Models/Login/User.php
+++ app/Models/Login/User.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\login;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable; 
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
+    protected $fillable = ['name', 'contact', 'email',  'password','googleId', 'facebookId','img_path','address', 'city', 'zip_code', 'user_type'];
+  
 }
