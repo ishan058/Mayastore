d6649df3b57b590310e97c6c806988909f8d9102
ishan058 <ishankhadkaik246@gmail.com>
2023-06-04 13:12:38 +0545

    solve

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'categoryName','price','image', 'status', 'description','category_id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
