3582bdf02a227a6e541341a17960892056a870ec
ishan058 <ishankhadkaik246@gmail.com>
2023-06-05 21:20:07 +0545

    home

--- app/Models/User/Home.php
+++ app/Models/User/Home.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\User\Home;
+use App\Models\Product\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class Home extends Model
 {
     use HasFactory;
+
+    protected $guarded=[];
+    
+    public function products(){
+        return $this->belongsTo(Product::class , 'productId' ,'id');
+    }
 }
